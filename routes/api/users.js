a67a41bda65388fb4df1ebf84ea9fdf76fb91e48
bernardomendonca <bernardomendonca@outlook.com>
2018-08-02 19:47:24 +1000

    added Login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -46,4 +46,29 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @route       GET request to api/users/login
+// @description Login user / returning JWT (json web token)
+// @access      Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email: email }).then(user => {
+    // Check if user exists
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "success" });
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
+  });
+});
+
 module.exports = router;
