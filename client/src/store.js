c57a9457e16403359d2558c7ef555eaaef8e32c9
bernardomendonca <bernardomendonca@outlook.com>
2018-08-20 18:54:38 +1000

    add Node version to package

--- client/src/store.js
+++ client/src/store.js
@@ -6,12 +6,13 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const store = createStore(
-  indexReducer,
+  rootReducer,
   initialState,
-  composeEnhancers(applyMiddleware(...middleware))
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
